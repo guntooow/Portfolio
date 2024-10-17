443db7d76666ec3035f561c5576bfac25de427ff
Gunterr <do3nt32144@gmail.com>
2024-10-17 20:35:54 -0300

    Finalização da HeroSection!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,23 @@ import { AppWindowMac as Projeto } from "lucide-react";
 import { Send as Contato } from "lucide-react";
 
 function Header() {
-    return(
-        <header className="z-50 pl-32 pr-32 flex sticky backdrop-blur-lg gap-96 p-8 bg-gray-200 bg-opacity-80 shadow-violet-300  shadow-lg w-screen justify-between">
-            <div className="flex text-purple-500 gap-3 font-bold text-xl opacity-80 cursor-pointer transition-all hover:opacity-100">
-                <Logo className="stroke-purple-500 scale-150"/>
+    return (
+        <header className="font-grotesk z-50 pl-32 pr-32 flex fixed backdrop-blur-lg gap-96 p-8 bg-gray-200 bg-opacity-90 shadow-violet-300  shadow-lg w-screen justify-between">
+            <div className="flex text-purple-600 gap-3 font-bold text-xl opacity-90 cursor-pointer transition-all hover:opacity-100">
+                <Logo className="stroke-purple-600 scale-150" />
                 <p>João_</p>
             </div>
-            <ul className="flex gap-8 right-0 text-purple-600 font-semibold">
-                <li className="opacity-80 hover:opacity-100 transition-all flex gap-2">
-                    <Sobre className="hover fill-purple-200"/>
+            <ul className="flex gap-8 right-0 text-purple-700 font-semibold">
+                <li className="opacity-90 hover:opacity-100 transition-all flex gap-2">
+                    <Sobre className="hover fill-purple-200" />
                     <a href="#sobre">Sobre mim</a>
                 </li>
-                <li className="opacity-80 hover:opacity-100 flex gap-2">
-                    <Projeto className="hover: fill-purple-200"/>
+                <li className="opacity-90 hover:opacity-100 flex gap-2">
+                    <Projeto className="hover: fill-purple-200" />
                     <a href="#projetos">Projetos</a>
                 </li>
-                <li className="opacity-80 hover:opacity-100 flex gap-2">
-                    <Contato className="hover: fill-purple-200"/>
+                <li className="opacity-90 hover:opacity-100 flex gap-2">
+                    <Contato className="hover: fill-purple-200" />
                     <a href="#contato">Contato</a>
                 </li>
             </ul>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,36 +1,41 @@
-import { BadgeAlert as BemVindo } from "lucide-react"
-import { GithubIcon } from "lucide-react"
-import { Linkedin } from "lucide-react"
-import { MessageCircleMore } from "lucide-react"
+import { BadgeAlert as BemVindo } from "lucide-react";
+import { GithubIcon } from "lucide-react";
+import { Linkedin } from "lucide-react";
+import { MessageCircleMore } from "lucide-react";
+import { Sparkles } from "lucide-react";
+import { MoonStar } from "lucide-react";
 
 // Imports aqui !!
 
 function HeroSection() {
     return (
-        <section className="bg-gray-200 bg-blend-darken shadow-sm shadow-gray-300 h-fit p-24 pl-32 pr-32 flex justify-between overflow-hidden gap-0">
-            <div className="flex-col space-y-3  align-middle">
-                <h1 className="text-purple-500 text-8xl font-bold">Olá! meu nome é <br/> João.</h1>
-                <p className="text-purple-400 text-4xl flex gap-1 max-w-4xl font-semibold">Desenvolvedor Web, usando JavaScript.</p>
-                <ul className="flex gap-3">
-                    <li>
-                        <a href="https://github.com/guntooow" target="blank">
-                            <GithubIcon className="stroke-purple-300 h-16 w-16 transition-all hover:stroke-purple-400 hover:scale-110"/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://br.linkedin.com/" target="blank">
-                            <Linkedin className="stroke-purple-300 h-16 w-16 transition-all hover:stroke-purple-400 hover:scale-110"/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://wa.me/5519971581846?text=Ol%C3%A1%2C%20estou%20interessado%20em%20te%20contratar%20como%20Desenvolvedor%20Web%20Front-End" target="blank">
-                            <MessageCircleMore className="stroke-purple-300 h-16 w-16 transition-all hover:stroke-purple-400 hover:scale-110"/>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <BemVindo className=" stroke-purple-500 h-96 w-96 p-0 m-0 scale-125 rotate-12"/>
-        </section>
+        <main className="pt-8">
+            <section className="font-grotesk bg-gray-200 bg-blend-darken shadow-sm shadow-gray-300 h-fit p-24 pl-32 pr-32 flex justify-between overflow-hidden gap-0 mt-10 relative">
+                <MoonStar className="absolute h-96 w-96 scale-150 rotate-12 mt-14 stroke-purple-400 opacity-30 z-0" />
+                <div className="flex-col space-y-3  align-middle z-10 relative p-16">
+                    <h1 className="text-purple-600 text-8xl z-10 font-bold">Olá! meu nome é <br /> João.</h1>
+                    <p className="text-purple-500 text-4xl z-10 flex gap-1 max-w-4xl font-semibold">Desenvolvedor Web, usando JavaScript.</p>
+                    <ul className="flex gap-3">
+                        <li>
+                            <a title="Meu Github!" href="https://github.com/guntooow" target="blank">
+                                <GithubIcon className="stroke-purple-400 h-16 w-16 transition-all hover:pb-2 hover:stroke-purple-500 hover:scale-110" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://br.linkedin.com/" target="blank" title="Meu linkedin!">
+                                <Linkedin className="stroke-purple-400 h-16 w-16 transition-all hover:pb-2 hover:stroke-purple-500 hover:scale-110" />
+                            </a>
+                        </li>
+                        <li>
+                            <a title="Meu Whatsapp!" href="https://wa.me/5519971581846?text=Ol%C3%A1%2C%20estou%20interessado%20em%20te%20contratar%20como%20Desenvolvedor%20Web%20Front-End" target="blank">
+                                <MessageCircleMore className="stroke-purple-400 h-16 w-16 transition-all hover:pb-2 hover:stroke-purple-500 hover:scale-110" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <BemVindo className=" stroke-purple-600 h-96 w-96 mt-8 p-0 m-0 scale-125 rotate-12" />
+            </section>
+        </main>
     )
 }
 
