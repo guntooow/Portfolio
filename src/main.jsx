7926e9b2655e643d467323b1e1c28bfedec47db3
Gunterr <do3nt32144@gmail.com>
2024-10-17 20:38:01 -0300

    Finalização da HeroSection

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './components/Header'
+import HeroSection from './components/HeroSection'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
+    <Header />
+    <HeroSection />
   </StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        grotesk: ['Hanken Grotesk', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
